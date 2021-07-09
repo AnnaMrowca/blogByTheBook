24b42ce62665ced85b2ba47d806f73ec63e68a12
AnnaMrowca <amrowca@gmail.com>
2021-07-09 22:36:03 +0200

    creacting class based view for Post

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,8 @@ app_name = 'blog' #app_name helps define name are of application, this allows or
 
 urlpatterns = [
     #views of post
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail')
 ]
 
